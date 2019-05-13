33ed5490517aefe871051764395578689d95f57d
Edson Lucas Carlotti <ecarlotti@dpvmdev.leadcomm.local>
2019-05-13 14:16:17 -0300

    added .gitignore

--- package.js
+++ package.js
@@ -9,18 +9,18 @@ Package.describe({
 
 Package.onUse(function (api) {
     api.versionsFrom('1.8.0.2');
-    api.use('jquery');
-    api.use('templating');
-    api.use('blaze');
+    api.use('jquery@1.11.11');
+    api.use('templating@1.3.2');
+    api.use('blaze@2.3.3');
     api.use('underscore');
     api.use('check');
     api.use('aldeed:autoform@6.3.0');
-    api.use('rajit:bootstrap3-datepicker');         // @1.1.1
-    api.use('tsega:bootstrap3-datetimepicker');     // @3.1.3_3
-    api.use('momentjs:moment');                     // @2.9.0
+    api.use('rajit:bootstrap3-datepicker@1.1.1');           // @1.1.1
+    api.use('tsega:bootstrap3-datetimepicker@3.1.3_3');     // @3.1.3_3
+    api.use('momentjs:moment@2.24.0');                      // @2.9.0
     api.use('vazco:universe-utilities@1.0.7');
-    api.use('reactive-dict');                       // @1.1.0
-    api.use('aramk:rrule');                         // @2.1.0
+    api.use('reactive-dict');                               // @1.1.0
+    api.use('aramk:rrule@2.1.0');                           // @2.1.0
 
     api.addFiles([
         'autoform-scheduler.html',
