6f0c24e5d1623dd2f3fd371898c4d9aa5eb0e4a3
ecarlotti <ecarlotti@hotmail.com>
2020-07-30 16:39:53 -0300

    removed version lock on npm dependencies

--- package.js
+++ package.js
@@ -3,23 +3,23 @@
 Package.describe({
     name: 'ecarlotti:meteor-autoform-scheduler',
     summary: 'Custom "afScheduler" input type for AutoForm',
-    version: '0.2.5',
+    version: '0.2.6',
     git: 'https://github.com/ecarlotti/meteor-autoform-scheduler.git'
 });
 
 Package.onUse(function (api) {
     api.versionsFrom('1.8.0.2');
-    api.use('jquery@1.11.11');
-    api.use('templating@1.3.2');
-    api.use('blaze@2.3.3');
+    api.use('jquery');                                      // @1.11.11 *
+    api.use('templating');                                  // @1.3.2 *
+    api.use('blaze');                                       // @2.3.3 *
     api.use('underscore');
     api.use('check');
-    api.use('aldeed:autoform@6.3.0');
-    api.use('momentjs:moment@2.24.0');                      // @2.9.0
-    api.use('vazco:universe-utilities@1.0.7');
+    api.use('aldeed:autoform');                             // @6.3.0 *
+    api.use('momentjs:moment');                             // @2.24.0 *
+    api.use('vazco:universe-utilities');                    // @1.0.7 *
     api.use('reactive-dict');                               // @1.1.0
-    api.use('aramk:rrule@2.1.0');                           // @2.1.0
-    api.use("tap:i18n@1.8.2");                              // https://atmospherejs.com/tap/i18n
+    api.use('aramk:rrule');                                 // @2.1.0 *
+    api.use("tap:i18n");                                    // @1.8.2 *
 
     api.addFiles([
         'lib/bootstrap3-datetimepicker/css/bootstrap-datetimepicker.css',   // from tsega:bootstrap3-datetimepicker
